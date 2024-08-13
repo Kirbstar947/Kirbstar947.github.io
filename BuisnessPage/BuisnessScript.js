9d5de31e889bb2ee68dabf42752d8482b298f9a1
JustinLiao <122176632+Kirbstar947@users.noreply.github.com>
2024-08-13 02:43:28 +0000

    10:40pm update, added about me page and fixed mis-alignment issues

---
+++ BuisnessPage/BuisnessScript.js
@@ -0,0 +1,10 @@
+let priceButton = document.querySelector('.infoButton');
+
+function infoAlert() {
+    alert("Under Construction! Sorry for the inconvenience.");
+}
+
+priceButton.addEventListener('click', infoAlert);
+
+
+// alert("The cards are made of metal to prevent bending and denting.\nThe cards also are water resistant to prevent marking.\nAnd who would want to mess up these sick cards?")
